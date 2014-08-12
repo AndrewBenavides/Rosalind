452f79bc7e9e7af653927635d4e45575447e46b5
Andrew Benavides <andrew.benavides@gmail.com>
2014-08-11 21:01:29 -0400

    Implemented solution for DNA.

---
+++ Core/DnaString.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Rosalind.Core {
+    public class DnaString {
+        public string Sequence { get; private set; }
+        
+        public DnaString(string sequence) {
+            this.Sequence = sequence;
+        }
+
+        private static List<int> GetNucleotideCounts(string sequence) {
+            return sequence
+                .OrderBy(n => n)
+                .GroupBy<char, char>(n => n)
+                .Select(g => g.Count())
+                .ToList();
+        }
+
+        public string GetNucleotideCounts() {
+            return string.Join(" ", GetNucleotideCounts(this.Sequence));
+        }
+    }
+}
