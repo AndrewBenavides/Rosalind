c7531154c033a15ab840e6ad007c9e1a4755bd8d
Andrew Benavides <andrew.benavides@gmail.com>
2014-08-12 21:30:10 -0400

    Implemented solution for GC.
    - Added Label property to DnaString
    - Added new constructor to DnaString for Label
    - Added overide for DnaString.ToString method
    - Created Database class, inherits Dictionary<string, DnaString>
    - Created Database.Parse method
    - Added QuickEquivalent method to Solutions
    - Organized usings for Nucleotide

---
+++ Core/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rosalind.Core {
+    public class Database : Dictionary<string, DnaString> {
+        private Database(Dictionary<string, DnaString> dictionary) : base(dictionary) { }
+
+        public static Database Parse(string databaseString) {
+            var remove = StringSplitOptions.RemoveEmptyEntries;
+            var sets = databaseString.Split(new char[] { '>' }, remove);
+            var sequences = sets
+                .Select(s => {
+                    var lines = s.Split(new char[] { '\r', '\n' }, remove);
+                    var label = lines.First();
+                    var sequence = string.Join("", lines.Skip(1));
+                    return new DnaString(label, sequence);
+                });
+            return new Database(sequences
+                .ToDictionary(s => s.Label, s => s));
+        }
+    }
+}
--- Core/DnaString.cs
+++ Core/DnaString.cs
@@ -3,10 +3,16 @@ using System.Linq;
 
 namespace Rosalind.Core {
     public class DnaString {
+        public string Label { get; private set; }
         public List<Nucleotide> Sequence { get; private set; }
         
         public DnaString(string sequenceString) {
             this.Sequence = GetSequence(sequenceString);
+        }
+
+        public DnaString(string label, string sequenceString)
+            : this(sequenceString) {
+                this.Label = label;
         }
 
         private static List<Nucleotide> GetSequence(string sequence) {
@@ -41,6 +47,16 @@ namespace Rosalind.Core {
             return string.Join("", this.Sequence
                 .Select(n => n.Complement.Symbol)
                 .Reverse());
+        }
+
+        public double GetGcContent() {
+            var matchingNucleotides = this.Sequence
+                .Count(n => n == Nucleotide.Cytosine || n == Nucleotide.Guanine);
+            return ((double)matchingNucleotides / this.Sequence.Count) * 100;
+        }
+
+        public override string ToString() {
+            return string.Join("", this.Sequence.Select(n => n.Symbol));
         }
     }
 }
