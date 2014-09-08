19a1f92e5d80564a90176f2b90b3759cb7923845
Andrew Benavides <andrew.benavides@gmail.com>
2014-09-08 11:43:25 -0400

    Testing different solutions for LCSM, work in progress.

--- Core/MotifFinder.cs
+++ Core/MotifFinder.cs
@@ -3,6 +3,80 @@ using System.Linq;
 
 namespace Rosalind.Core {
     public static class MotifFinder {
+        #region SuffixArray
+        public static List<string> GenerateSuffixArray(Sequence sequence) {
+            var seq = sequence.ToString();
+            var suffix = new SortedSet<string>(GetSuffixes(seq));
+            return suffix.ToList();
+        }
+
+        private static IEnumerable<string> GetSuffixes(string value) {
+            var len = value.Length;
+            for (int i = 0; i < len; i++) {
+                yield return value.Substring(i, len - i);
+            }
+        }
+
+        private static IEnumerable<string> ExplodeString(string value) {
+            var len = value.Length;
+            for (int i = len; i >= 2; i--) {
+                for (int j = 0; j <= len - i; j++) {
+                    yield return value.Substring(j, i);
+                }
+            }
+        }
+
+        //http://rosalind.info/problems/lcsm/solutions/#comment-259
+        public static List<Sequence> FindMotifs(IEnumerable<Sequence> sequences) {
+            var remaining = sequences.Skip(1)
+                .Select(s => GenerateSuffixArray(s))
+                .ToList();
+            var combos = ExplodeString(sequences.First().ToString()).ToList();
+            var common = combos
+                .Distinct()
+                .Where(sub => remaining.All(s => Contains(s, sub)))
+                .Select(s => Sequence.Parse(s))
+                .ToList();
+            return common; ;
+        }
+        #endregion
+
+        #region Reduce
+        private static bool Contains(List<string> suffix, string value) {
+            var result = suffix.BinarySearch(value);
+            if (result >= 0) return true;
+            if (~result >= suffix.Count) return false;
+            return suffix[~result].Contains(value);
+        }
+
+        private static IEnumerable<string> CommonSubStr(IList<string> strings, int frameLength) {
+            var first = strings[0];
+            var range = first.Length - frameLength + 1;
+            for (int i = 0; i < range; i++) {
+                var part = first.Substring(i, frameLength);
+                if (strings.Skip(1).All(s => s.Contains(part))) yield return part;
+            }
+        }
+
+        //http://rosalind.info/problems/lcsm/solutions/#comment-192
+        public static IList<string> FindLongestCommonSubstrings(IEnumerable<string> strings) {
+            var sorted = strings.OrderByDescending(s => s.Length).ToList();
+            var min = 0;
+            var max = sorted[0].Length;
+            var mid = max;
+            do {
+                if (CommonSubStr(sorted, mid).Any()) {
+                    min = mid;
+                } else {
+                    max = mid;
+                }
+                mid = (min + max) / 2;
+            } while (min + 1 < max);
+            return CommonSubStr(sorted, mid).ToList();
+        }
+        #endregion
+
+        #region Brute Force
         private static IEnumerable<Sequence> ExplodeSequence(Sequence sequence) {
             for (int i = sequence.Count; i >= 2; i--) {
                 for (int j = 0; j <= sequence.Count - i; j++) {
@@ -38,5 +112,6 @@ namespace Rosalind.Core {
             }
             return locations;
         }
+        #endregion
     }
 }
